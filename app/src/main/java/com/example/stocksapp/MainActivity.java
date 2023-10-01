3f34aa8b7c43151d936acdb066512eea536e83e7
carsondrobe <carsondrobe@gmail.com>
2023-10-01 14:58:55 -0700

    Retrieve Daily Close info for any Stock

--- app/src/main/java/com/example/stocksapp/MainActivity.java
+++ app/src/main/java/com/example/stocksapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.stocksapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,45 +24,47 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView SnP500Close;
+    private TextView tickerClose;
     private Spinner currencySpinner;
+
+    private Button viewResults;
+
+    private EditText tickerSymbolView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         currencySpinner = findViewById(R.id.currencySpinner);
-        retrieveCloseInfo();
-        currencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        viewResults = (Button) findViewById(R.id.resultsBtn);
+        tickerSymbolView = (EditText) findViewById(R.id.ticker);
+        currencySpinner.setSelection(0);
+        viewResults.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                retrieveCloseInfo();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                retrieveCloseInfo();
+            public void onClick(View view) {
+                String ticker;
+                ticker = (String) tickerSymbolView.getText().toString();
+                retrieveCloseInfo(ticker);
             }
         });
     }
 
-    protected void retrieveCloseInfo() {
-        SnP500Close = findViewById(R.id.priceLabel);
+    protected void retrieveCloseInfo(String ticker) {
+        tickerClose = findViewById(R.id.priceLabel);
 
         OkHttpClient client = new OkHttpClient();
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        String url = "https://api.tiingo.com/tiingo/daily/SPY/prices?token=b8eeb0525ea6b650cb6915f3361fda61e7628ba0";
+        String url = "https://api.tiingo.com/tiingo/daily/" + ticker + "/prices?token=b8eeb0525ea6b650cb6915f3361fda61e7628ba0";
 
         Request request = new Request.Builder()
                 .url(url)
                 .addHeader("Content-Type", "application/json")
                 .build();
 
-
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.printStackTrace();
+                tickerClose.setText("Ticker Unavailable");
             }
 
             @Override
@@ -74,7 +78,8 @@ public class MainActivity extends AppCompatActivity {
                     if (selectedCurrency.equals("USD")) {
                         closeValue = String.valueOf(stockDataArray[0].getClose());
                     } else if (selectedCurrency.equals("CAD")) {
-                        closeValue = String.valueOf(1.35 * stockDataArray[0].getClose());
+                        double closeCAD = 1.35 * stockDataArray[0].getClose();
+                        closeValue = String.format("%.2f", closeCAD);
                     } else {
                         closeValue = "Error";
                     }
@@ -82,7 +87,17 @@ public class MainActivity extends AppCompatActivity {
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            SnP500Close.setText(closeValue);
+                            tickerClose.setText(closeValue);
+                        }
+                    });
+                } else if (response.code() == 404) {
+                    // Handle a 404 Not Found error
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tickerClose.setText("###.##");
+                            Toast.makeText(getApplicationContext(), "Sorry, Ticker Unavailable at This Time",
+                                    Toast.LENGTH_LONG).show();
                         }
                     });
                 }
--- app/src/main/java/com/example/stocksapp/Stock.java
+++ app/src/main/java/com/example/stocksapp/Stock.java
@@ -2,8 +2,12 @@ package com.example.stocksapp;
 
 public class Stock {
     private double close;
+    private String detail;
     public double getClose() {
         return close;
     }
 
+    public String getDetail() {
+        return detail;
+    }
 }
